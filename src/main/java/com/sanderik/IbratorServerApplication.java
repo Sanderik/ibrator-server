18c4efd991c4c9a2f20dc8327ef70b9d7b5df29a
Sander de Winter <sanderdewinter@gmail.com>
2016-10-10 09:59:40 +0200

    spring-boot init

---
+++ src/main/java/com/sanderik/IbratorServerApplication.java
@@ -0,0 +1,12 @@
+package com.sanderik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbratorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbratorServerApplication.class, args);
+	}
+}
