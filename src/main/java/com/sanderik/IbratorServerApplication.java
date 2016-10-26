c933f380f9490990003c922664b260736812407d
Sander de Winter <sanderdewinter@gmail.com>
2016-10-26 15:24:31 +0200

    setup firmware controller

--- src/main/java/com/sanderik/IbratorServerApplication.java
+++ src/main/java/com/sanderik/IbratorServerApplication.java
@@ -1,11 +1,15 @@
 package com.sanderik;
 
+import com.sanderik.storage.StorageProperties;
+import com.sanderik.storage.StorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +20,7 @@ import java.util.List;
 
 @EnableAutoConfiguration
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class IbratorServerApplication extends SpringBootServletInitializer {
 
 	private static final Logger log = LoggerFactory.getLogger(IbratorServerApplication.class);
@@ -29,4 +34,10 @@ public class IbratorServerApplication extends SpringBootServletInitializer {
 		return application.sources(IbratorServerApplication.class);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.init();
+		};
+	}
 }
